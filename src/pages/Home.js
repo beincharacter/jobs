6699245804e137ebdff7e80d07685794cf673632
shubhampalgaian <shubhamm.pal@gaiansolutions.com>
2024-06-01 13:19:40 +0530

    List view and card vire for application

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../scss/Home.scss";
 import { auth, provider, signInWithPopup } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import FirebaseService from "../utils/firebaseService";
+import { useOrganization } from "../utils/OrganizationContext";
+import { toast } from "react-toastify";
 
 export const Home = () => {
     const navigate = useNavigate();
+    const { setORgDetails, setLoading } = useOrganization();
+
+    useEffect(() => {
+        const checkExistingOrganization = async () => {
+            setLoading(true);
+            const email = localStorage.getItem("userEmail");
+    
+            if (!email) {
+                console.error("No user email found.");
+                setLoading(false);
+                return;
+            }
+    
+            try {
+                const exists = await FirebaseService.checkOrganizationExistsByEmail(email);
+                console.log({exists});
+                await setORgDetails(exists[0]);
+                localStorage.setItem("id", exists[0].id);
+                if (exists) {
+                    setLoading(false);
+                    navigate('/dashboard');
+                    toast.success("Login success");
+                }
+            } catch (error) {
+                setLoading(false);
+                console.error("Error checking existing organization: ", error);
+            }
+        };
+    
+        checkExistingOrganization();
+    })
 
     const handleLogin = async () => {
         try {
             const result = await signInWithPopup(auth, provider);
             const user = result.user;
             const email = user.email;
-            
+
             localStorage.setItem("userEmail", email);
 
             navigate('/register');
