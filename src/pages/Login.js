ac00fd6f517adbdc90b5b68f9c49d417782e7746
shubhampal <shubhampalatwork@gmail.com>
2024-06-07 23:16:10 +0530

    Started desiging landing page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export const Login = () => {
     const cardRef = useRef(null);
@@ -38,50 +40,39 @@ export const Login = () => {
 
     return (
         <>
-            <div className="relative flex flex-col gap-[100px] items-center w-screen h-screen bg-gradient-to-r from-gray-900 via-black to-gray-900 overflow-y-auto custom-scrollbar">
+            <div className="relative flex flex-col items-center w-screen h-screen overflow-y-auto custom-scrollbar">
 
-                {/* on scroll header   */}
-                <header className={`fixed top-0 right-0 flex space-x-8 text-lg rounded-full border border-[#ffffff28] p-4 mt-4 items transition-transform duration-500 w-fit left-1/2 -translate-x-1/2 bg-blue-200 text-black  ${isVisible ? '-translate-y-52' : 'translate-y-0'}`}>
+                {/* on scroll header */}
+                <header className={`fixed top-0 right-0 flex space-x-8 text-lg rounded-full border border-[#ffffff28] p-4 mt-4 items transition-transform duration-500 w-fit left-1/2 -translate-x-1/2 bg-blue-200 text-black ${isVisible ? '-translate-y-52' : 'translate-y-0'}`}>
                     <nav className="cursor-pointer hover p-1">Home</nav>
                     <nav className="cursor-pointer hover p-1">Jobs</nav>
                     <nav className="cursor-pointer hover p-1">About</nav>
                     <nav className="cursor-pointer hover p-1">Contacts</nav>
                 </header>
 
-                {/* on load header  */}
-                <header className="flex justify-between items-center p-4 w-full pt-4">
+                {/* on load header */}
+                <header className="flex justify-between items-center p-4 w-full pt-4 bg-[#FFFF82] text-black">
                     <div className="flex items-center">
-                        <img src="path/to/logo.png" alt="Logo" className="h-8 w-8 mr-2" />
-                        <div className="text-white text-lg">Logo</div>
+                        <img src="https://via.placeholder.com/32" alt="Logo" className="h-8 w-8 mr-2" />
+                        {/* <div className="text-black text-lg">Logo</div> */}
                     </div>
-                    <div className="flex space-x-8 text-white text-lg rounded-full border border-[#ffffff28] p-4">
-                    <nav className="cursor-pointer hover p-1">Home</nav>
-                    <nav className="cursor-pointer hover p-1">Jobs</nav>
-                    <nav className="cursor-pointer hover p-1">About</nav>
-                    <nav className="cursor-pointer hover p-1">Contacts</nav>
+                    <div className="flex space-x-8 text-black text-lg rounded-full border border-[#ffffff28] p-4">
+                        <nav className="cursor-pointer hover p-1">Home</nav>
+                        <nav className="cursor-pointer hover p-1">Jobs</nav>
+                        <nav className="cursor-pointer hover p-1">About</nav>
+                        <nav className="cursor-pointer hover p-1">Contacts</nav>
                     </div>
                     <div className="flex items-center">
-                        <img src="path/to/logo.png" alt="Logo" className="h-8 w-8 mr-2" />
-                        <div className="text-white text-lg">User</div>
+                        <FontAwesomeIcon icon={faUser} className="h-8 w-8 mr-2" />
+                        {/* <div className="text-black text-lg">User</div> */}
                     </div>
                 </header>
 
-                <div className="flex w-9/12 h-[600px] min-h-[600px] border border-[#63484828] text-white p-4 rounded-lg"
-                    ref={cardRef}
-                >
-                    <span></span>
+                <div className="flex w-full h-[600px] bg-[#FFFF82] min-h-[600px] text-black p-4" ref={cardRef}>
+                    <span>Connect. Collaborate. Conquer Your Career</span>
                     <div></div>
                 </div>
-
-                <div className="flex w-9/12 h-[600px] min-h-[600px] border border-[#ffffff28] text-white p-4 rounded-lg">
-                    <span></span>
-                    <div></div>
-                </div>
-
-                <div className="flex w-9/12 h-[600px] min-h-[600px] border border-[#ffffff28] text-white p-4 rounded-lg">
-                    <span></span>
-                    <div></div>
-                </div>
+                
             </div>
         </>
     );
