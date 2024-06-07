7b561b9a30958e89939cb915f069230b1f6e4565
shubhampal <shubhampalatwork@gmail.com>
2024-06-07 21:18:47 +0530

    added onhover bg

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -39,23 +39,26 @@ export const Login = () => {
     return (
         <>
             <div className="relative flex flex-col gap-[100px] items-center w-screen h-screen bg-gradient-to-r from-gray-900 via-black to-gray-900 overflow-y-auto custom-scrollbar">
-                <header className={`fixed top-0 right-0 flex space-x-8 text-white text-lg rounded-full border border-[#ffffff28] p-4 mt-4 items transition-transform duration-500 w-fit left-1/2 -translate-x-1/2  ${isVisible ? '-translate-y-52' : 'translate-y-0'}`}>
-                    <nav>Home</nav>
-                    <nav>Jobs</nav>
-                    <nav>About</nav>
-                    <nav>Contacts</nav>
+
+                {/* on scroll header   */}
+                <header className={`fixed top-0 right-0 flex space-x-8 text-lg rounded-full border border-[#ffffff28] p-4 mt-4 items transition-transform duration-500 w-fit left-1/2 -translate-x-1/2 bg-blue-200 text-black  ${isVisible ? '-translate-y-52' : 'translate-y-0'}`}>
+                    <nav className="cursor-pointer hover p-1">Home</nav>
+                    <nav className="cursor-pointer hover p-1">Jobs</nav>
+                    <nav className="cursor-pointer hover p-1">About</nav>
+                    <nav className="cursor-pointer hover p-1">Contacts</nav>
                 </header>
 
+                {/* on load header  */}
                 <header className="flex justify-between items-center p-4 w-full pt-4">
                     <div className="flex items-center">
                         <img src="path/to/logo.png" alt="Logo" className="h-8 w-8 mr-2" />
                         <div className="text-white text-lg">Logo</div>
                     </div>
                     <div className="flex space-x-8 text-white text-lg rounded-full border border-[#ffffff28] p-4">
-                        <nav>Home</nav>
-                        <nav>Jobs</nav>
-                        <nav>About</nav>
-                        <nav>Contacts</nav>
+                    <nav className="cursor-pointer hover p-1">Home</nav>
+                    <nav className="cursor-pointer hover p-1">Jobs</nav>
+                    <nav className="cursor-pointer hover p-1">About</nav>
+                    <nav className="cursor-pointer hover p-1">Contacts</nav>
                     </div>
                     <div className="flex items-center">
                         <img src="path/to/logo.png" alt="Logo" className="h-8 w-8 mr-2" />
@@ -63,7 +66,7 @@ export const Login = () => {
                     </div>
                 </header>
 
-                <div className="flex w-9/12 h-[600px] min-h-[600px] border border-[#63484828] text-white p-4 rounded-lg bg-blue-300"
+                <div className="flex w-9/12 h-[600px] min-h-[600px] border border-[#63484828] text-white p-4 rounded-lg"
                     ref={cardRef}
                 >
                     <span></span>
